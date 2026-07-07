df33c64040bae8a3f03041386dc85a25fc2a24f8
sajan-kumar-singh <saajan.199923@gmail.com>
2026-07-07 16:03:35 +0530

    register api working

--- backend/src/main/java/com/feuji/backend/BackendApplication.java
+++ backend/src/main/java/com/feuji/backend/BackendApplication.java
@@ -2,12 +2,19 @@ package com.feuji.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.TimeZone;
+import jakarta.annotation.PostConstruct;
 
 @SpringBootApplication
 public class BackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
-	}
+    @PostConstruct
+    public void init() {
+        // Force the JVM to use Asia/Kolkata
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Kolkata"));
+    }
 
+    public static void main(String[] args) {
+        SpringApplication.run(BackendApplication.class, args);
+    }
 }
